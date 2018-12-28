e9b8a355c5315bfeeb69f5d880308062acf90448
theoners <theonebgrs@gmail.com>
2018-12-29 01:04:55 +0200

    added Csharp Advanced Exam - 14 October 2018

---
+++ Csharp Advanced Exam - 14 October 2018/01. Data Transfer/DataTransfer.cs
@@ -0,0 +1,46 @@
+﻿namespace _01._Data_Transfer
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class DataTransfer
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex senderRegex = new Regex(@"s:(.+?);");
+            Regex receiverRegex = new Regex(@"r:(.+?);");
+            Regex messageRegex = new Regex(@"m--(""[A-Za-z ]+"")");
+
+            var dataTransfer = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                bool isValidInput = senderRegex.IsMatch(input) && receiverRegex.IsMatch(input) && messageRegex.IsMatch(input);
+                if (isValidInput)
+                {
+                    var nameRegex = new Regex(@"[A-Za-z ]");
+                    var senderMatches = nameRegex.Matches(senderRegex.Match(input).Groups[1].Value);
+                    var receiverMatches = nameRegex.Matches(receiverRegex.Match(input).Groups[1].Value);
+
+
+                    for (int j = 0; j < input.Length; j++)
+                    {
+                        if (char.IsDigit(input[j]))
+                        {
+                            dataTransfer += input[j]-48;
+                        }
+                    }
+                    Console.Write(string.Join("", senderMatches));
+                    Console.Write($" says ");
+                    Console.Write(messageRegex.Match(input).Groups[1].Value+" to ");
+                    Console.Write(string.Join("", receiverMatches));
+                    Console.WriteLine();
+                    
+                }
+            }
+            Console.WriteLine($"Total data transferred: {dataTransfer}MB");
+        }
+    }
+}
---
+++ Csharp Advanced Exam - 14 October 2018/02. Tagram/Tagram.cs
@@ -0,0 +1,60 @@
+﻿namespace _02._Tagram
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Tagram
+    {
+        public static void Main()
+        {
+            var tagram = new Dictionary<string, Dictionary<string, int>>();
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+
+                if (input.Contains(" -> "))
+                {
+                    var tagramElements = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    var name = tagramElements[0];
+                    var tag = tagramElements[1];
+                    var likes = int.Parse(tagramElements[2]);
+
+                    if (!tagram.ContainsKey(name))
+                    {
+                        tagram.Add(name, new Dictionary<string, int>());
+                    }
+                    if (!tagram[name].ContainsKey(tag))
+                    {
+                        tagram[name].Add(tag, 0);
+                    }
+
+                    tagram[name][tag] += likes;
+                }
+                else
+                {
+                    var bannedUsername = input.Split()[1];
+
+                    if (tagram.ContainsKey(bannedUsername))
+                    {
+                        tagram.Remove(bannedUsername);
+                    }
+                }
+            }
+
+            foreach (var users in tagram.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x => x.Value.Count))
+            {
+                Console.WriteLine($"{users.Key}");
+
+                foreach (var tagsAndLikes in users.Value)
+                {
+                    Console.WriteLine($"- {tagsAndLikes.Key}: {tagsAndLikes.Value}");
+                }
+            }
+        }
+    }
+}
