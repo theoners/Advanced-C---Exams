818f81be4052810b5487334bc11f406c7a770a05
theoners <theonebgrs@gmail.com>
2019-01-13 20:38:35 +0100

    added Csharp Advanced Exam Retake - 24 April 2018

---
+++ Csharp Advanced Exam Retake - 24 April 2018/03. Ticket Trouble/TIcketTrouble.cs
@@ -0,0 +1,93 @@
+﻿namespace _03._Ticket_Trouble
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class TicketTrouble
+    {
+        public static void Main()
+        {
+            var destination = Console.ReadLine();
+            var input = Console.ReadLine();
+            var seatPlaces = new List<string>();
+            Regex regex = new Regex(@"([{\[])[^{}\[\]]*([{\[])([A-Z]+ [A-Z]+)([}\]])[^{}\[\]]*([\[{])([A-Z][0-9]{1,2})([\]}])[^{}\[\]]*([\]}])");
+
+            var matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                bool isValid = ValidTicket(match, destination);
+
+                if (isValid)
+                {
+                    seatPlaces.Add(match.Groups[6].Value);
+
+                }
+
+            }
+
+            var sortedList = seatPlaces.OrderByDescending(x =>seatPlaces.Count(y => y.Substring(1) == x.Substring(1))).ToList();
+            if (seatPlaces.Count==2)
+            {
+                Console.WriteLine($"You are traveling to {destination} on seats {seatPlaces[0]} and {seatPlaces[1]}.");
+            }
+            else
+            {
+                Console.WriteLine($"You are traveling to {destination} on seats {sortedList[0]} and {sortedList[1]}.");
+            }
+            
+        }
+
+        private static bool ValidTicket(Match match, string destination)
+        {
+            var firstBracket = match.Groups[1].Value;
+            var secondBracket = match.Groups[2].Value;
+            var matchDestination = match.Groups[3].Value;
+            var thirdBracket = match.Groups[4].Value;
+            var fourthBracket = match.Groups[5].Value;
+            var fifthBracket = match.Groups[7].Value;
+            var sixthBracket = match.Groups[8].Value;
+
+            if (matchDestination!=destination)
+            {
+                return false;
+            }
+
+            bool firstGroupBrackets = ValidBrackets(firstBracket,sixthBracket);
+            bool secondGroupBrackets = ValidBrackets(secondBracket,thirdBracket);
+            bool thirdGroupBrackets = ValidBrackets(fourthBracket,fifthBracket);
+            if (firstGroupBrackets&&secondGroupBrackets&&thirdGroupBrackets)
+            {
+                if (firstBracket!=secondBracket|| firstBracket!=fourthBracket)
+                {
+                    return true;
+                }
+                
+            }
+
+            return false;
+        }
+
+        private static bool ValidBrackets(string openBracket, string closeBracket)
+        {
+            if (openBracket == "{")
+            {
+                if (closeBracket != "}")
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (closeBracket != "]")
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Csharp Advanced Exam Retake - 24 April 2018/04. Movie Time/MovieTime.cs
@@ -0,0 +1,81 @@
+﻿namespace _04._Movie_Time
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public TimeSpan Time { get; set; }
+
+    }
+    public class MovieTime
+    {
+        public static void Main()
+        {
+            var movies = new List<Movie>();
+            string genre = Console.ReadLine();
+            string duration = Console.ReadLine();
+            var totalTime = new TimeSpan();
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input== "POPCORN!")
+                {
+                    break;
+                }
+
+                var movie = input.Split('|');
+                var movieName = movie[0].Trim();
+                var movieGenre = movie[1].Trim();
+                var movieDuration = movie[2].Split(':').Select(int.Parse).ToArray();
+                TimeSpan time = new TimeSpan(movieDuration[0],movieDuration[1],movieDuration[2]);
+                totalTime += time;
+                var currentMovie = new Movie()
+                {
+                    Name = movieName,
+                    Genre = movieGenre,
+                    Time = time
+                };
+                movies.Add(currentMovie);
+            }
+
+            if (duration=="Short")
+            {
+                foreach (var movie in movies.OrderBy(x=>x.Time))
+                {
+                    if (genre==movie.Genre)
+                    {
+                        Console.WriteLine(movie.Name);
+                        var answer = Console.ReadLine();
+                        if (answer == "Yes")
+                        {
+                            Console.WriteLine($"We're watching {movie.Name} - {movie.Time}");
+                            Console.WriteLine($"Total Playlist Duration: {totalTime}");
+                            return;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var movie in movies.OrderByDescending(x => x.Time))
+                {
+                    if (genre == movie.Genre)
+                    {
+                        Console.WriteLine(movie.Name);
+                        var answer = Console.ReadLine();
+                        if (answer == "Yes")
+                        {
+                            Console.WriteLine($"We're watching {movie.Name} - {movie.Time}");
+                            Console.WriteLine($"Total Playlist Duration: {totalTime}");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
