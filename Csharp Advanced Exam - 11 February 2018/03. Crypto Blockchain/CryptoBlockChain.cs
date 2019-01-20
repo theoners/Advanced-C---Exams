eb9566cf5d63ae9c88deded13750389fead31f0d
theoners <theonebgrs@gmail.com>
2019-01-20 20:11:21 +0100

    added Csharp Advanced Exam - 11 February 2018

---
+++ Csharp Advanced Exam - 11 February 2018/03. Crypto Blockchain/CryptoBlockChain.cs
@@ -0,0 +1,64 @@
+﻿namespace _03._Crypto_Blockchain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class CryptoBlockChain
+    {
+        public static void Main()
+        {
+            int lineCount = int.Parse(Console.ReadLine());
+            var text = string.Empty;
+            for (int i = 0; i < lineCount; i++)
+            {
+                text += Console.ReadLine();
+            }
+
+            var result = string.Empty;
+            var regex = new Regex(@"([{[])(.*?(\d{3,}).*?)([]}])");
+            var matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                var openBracket = match.Groups[1].Value;
+                var textCount = match.Length;
+                var allDigits = match.Groups[3].Value;
+                var closeBracket = match.Groups[4].Value;
+
+                if (CheckBrackets(openBracket,closeBracket)&& allDigits.Length%3==0)
+                {
+                    var chars = new Queue<char>();
+                    while (allDigits.Length!=0)
+                    {
+                        var digit = int.Parse(allDigits.Substring(0,3));
+                        allDigits = allDigits.Substring(3,allDigits.Length-3);
+                        digit -= textCount;
+                        chars.Enqueue((char)digit);
+                    }
+
+                    while (chars.Any())
+                    {
+                        result += chars.Dequeue();
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static bool CheckBrackets(string openBracket, string closeBracket)
+        {
+            if (openBracket=="[" && closeBracket=="]")
+            {
+                return true;
+            }
+            else if (openBracket == "{" && closeBracket == "}")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Csharp Advanced Exam - 11 February 2018/04. Hit List/HitList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Hit_List
+{
+    public class HitList
+    {
+        public static void Main()
+        {
+            var targetInfoIndex = int.Parse(Console.ReadLine());
+            var personsInfo = new Dictionary<string, SortedDictionary<string, string>>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input== "end transmissions")
+                {
+                    break;
+                }
+
+                var info = input.Split(new[] {":", "=",";"}, StringSplitOptions.RemoveEmptyEntries);
+
+                var name = info[0];
+                if (!personsInfo.ContainsKey(name))
+                {
+                    personsInfo.Add(name, new SortedDictionary<string, string>());
+                }
+                for (int i = 1; i < info.Length; i+=2)
+                {
+                        var keyInfo = info[i];
+                        if (!personsInfo[name].ContainsKey(keyInfo))
+                        {
+                            personsInfo[name].Add(keyInfo,"");
+                        }
+                        var valueInfo = info[i+1];
+                        personsInfo[name][keyInfo] = valueInfo;
+                }
+            }
+
+            var nameInfo = Console.ReadLine().Split()[1];
+            var symbolsCounts = 0;
+            Console.WriteLine($"Info on {nameInfo}:");
+            foreach (var personInfo in personsInfo.Where(x=>x.Key==nameInfo))
+            {
+                foreach (var kvp in personInfo.Value)
+                {
+                    Console.WriteLine($"---{kvp.Key}: {kvp.Value}");
+                    symbolsCounts += kvp.Key.Length + kvp.Value.Length;
+                }
+            }
+
+            Console.WriteLine($"Info index: {symbolsCounts}");
+            if (symbolsCounts>=targetInfoIndex)
+            {
+                Console.WriteLine("Proceed");
+            }
+            else
+            {
+                Console.WriteLine($"Need {targetInfoIndex-symbolsCounts} more info.");
+            }
+        }
+    }
+}
